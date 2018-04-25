9e5195dbccf1e85fcccb8adbcba62b6a376accda
naveen bollimpalli <naveenbollimpalli@naveens-MacBook-Pro.local>
2018-04-26 03:09:47 +0530

    added fabric

---
+++ fabfile.py
@@ -0,0 +1,55 @@
+import random
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+
+REPO_URL = 'https://naveen_bollimpalli@bitbucket.org/algovent/petcarehandler.git'
+SERVER_CONFIG_URL = ''
+env.hosts = ['ec2-13-232-13-243.ap-south-1.compute.amazonaws.com']
+env.user = 'ubuntu'
+env.key_filename = '~/certs/petcareHandler.pem'
+
+def deploy():
+    _setup_prereq()
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
+
+def _setup_prereq():
+    #check if pip
+
+def setup_server_configs():
+
+
+def _get_latest_source():
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run(f'git clone {REPO_URL} .')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
+
+def _update_virtualenv():
+    if not exists('prodenv/bin/pip'):
+        run(f'python3 -m venv prodenv')
+    run('./prodenv/bin/pip install -r requirements.txt')
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
+def _update_static_files():
+    run('./prodenv/bin/python manage.py collectstatic --noinput')
+
+def _update_database():
+    run('./prodenv/bin/python manage.py migrate --noinput')
